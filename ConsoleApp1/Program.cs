28237e86de5f4a5417c199395c2a86551543e79a
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-21 12:13:38 -0600

    Updated step 199 with comments.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,22 +12,28 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Car Insurance Approval Form");
 
+            // Getting input from user and assigning it to an integer.
             Console.WriteLine("What is your age?");
             string age = Console.ReadLine();
             int userAge = Convert.ToInt32(age);
 
+            // Getting input from user and assigning it to a boolean.
             Console.WriteLine("Have you ever had a DUI? Please enter \"true\" for YES or \"false\" for NO.");
             string dui = Console.ReadLine();
             bool userDui = Convert.ToBoolean(dui);
 
+            // Getting input from user and assigning it to an integer.
             Console.WriteLine("How many speeding tickets do you have?");
             string speedingTickets = Console.ReadLine();
             int userSpeedingTickets = Convert.ToInt32(speedingTickets);
 
+            // Printing the age, DUI status, and speeding tickets of the user.
             Console.WriteLine("Your age: " + userAge);
             Console.WriteLine("DUI status: " + userDui);
             Console.WriteLine("Speeding tickets: " + userSpeedingTickets + "\n");
 
+            // If else loop to check for qualifying or disqualifying conditions with resulting, respective text printed
+            // to screen.
             if (userAge >= 15 && userDui == false && userSpeedingTickets <= 3)
             {
                 Console.WriteLine("Congratulations! You qualify for auto insurance.");
