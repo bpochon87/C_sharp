fa30988c05c8c891143627a9b3c8a7047303a53d
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-23 10:19:04 -0600

    Step 229

---
+++ Practice/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Practice
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 62:
+                    Console.WriteLine("You guessed 62! Wrongo! Guess again!");
+                    break;
+                case 29:
+                    Console.WriteLine("You guessed 29! Wrongo! Guess again!");
+                    break;
+                case 7:
+                    Console.WriteLine("You guessed 7! Nope. Try again!");
+                    break;
+                case 12:
+                    Console.WriteLine("You guessed 12 and that's right!");
+                    break;
+                default:
+                    Console.WriteLine("You suck at guessing numbers.");
+                    break;
+                    
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ step229/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace step229
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 7;
+
+            do
+            {
+                switch (number)
+                {
+                    case 5:
+                        Console.WriteLine("5 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 10:
+                        Console.WriteLine("10 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 15:
+                        Console.WriteLine("15 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 8:
+                        Console.WriteLine("8 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 7:
+                        Console.WriteLine("7 is correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Keep trying.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (isGuessed);
+            Console.Read();
+        }
+            
+    }
+}
