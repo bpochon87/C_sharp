1b19a4fee5e63f92e2be54a26ebed155b71c55cf
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-23 16:13:14 -0600

    Updated step229

--- step229/Program.cs
+++ step229/Program.cs
@@ -4,30 +4,54 @@ namespace step229
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Guess a number: ");
+            Console.WriteLine("Guess a number under 10: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = number == 7;
+            while (number <= 0 || number > 10)
+            {
+                Console.WriteLine(number + " is not a valid number for this game!\n");
+                Main();
+                break;
+            }
+            bool isGuessed = false;
 
             do
             {
                 switch (number)
                 {
+                    case 1:
+                        Console.WriteLine("1 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("2 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("3 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 4:
+                        Console.WriteLine("4 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
                     case 5:
                         Console.WriteLine("5 is incorrect.");
                         Console.WriteLine("Please guess again:");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
 
-                    case 10:
-                        Console.WriteLine("10 is incorrect.");
-                        Console.WriteLine("Please guess again:");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-
-                    case 15:
-                        Console.WriteLine("15 is incorrect.");
+                    case 6:
+                        Console.WriteLine("6 is incorrect.");
                         Console.WriteLine("Please guess again:");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
@@ -38,19 +62,25 @@ namespace step229
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
 
+                    case 9:
+                        Console.WriteLine("9 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 10:
+                        Console.WriteLine("10 is incorrect.");
+                        Console.WriteLine("Please guess again:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
                     case 7:
                         Console.WriteLine("7 is correct!");
                         isGuessed = true;
                         break;
-
-                    default:
-                        Console.WriteLine("Keep trying.");
-                        Console.WriteLine("Please guess again:");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
                 }
             }
-            while (isGuessed);
+            while (isGuessed == false);
             Console.Read();
         }
             
