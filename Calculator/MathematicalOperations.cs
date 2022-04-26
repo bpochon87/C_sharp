965871146982be319bc69b55f6d86a77ad2afc03
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-26 11:30:42 -0600

    Step 275 help.

---
+++ Calculator/MathematicalOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MathematicalOperations
+    {
+        public MathematicalOperations(int userNumOne, int userNumTwo)
+        {
+            // Addition
+            public static MathematicalOperations Add()
+            {
+                int sum = userNumOne + userNumTwo;
+                return sum;
+            }
+
+            // Subtraction
+            public static MathematicalOperations Subtract()
+            {
+                int difference = userNumOne - userNumTwo;
+                return difference;
+            }
+
+            // Multiplication
+            public static MathematicalOperations Multiply()
+            {
+                int product = userNumOne * userNumTwo;
+                return product;
+            }
+
+            // Division
+            public static MathematicalOperations Divide()
+            {
+                int quotient = userNumOne / userNumTwo;
+                return quotient;
+            }
+
+        }
+
+        public int UserNumOne { get; }
+        public int UserNumTwo { get; }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int userNumOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number: ");
+            int userNumTwo = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiating new object of 'MathetmaticalOperations' class.
+            MathematicalOperations calculate = new MathematicalOperations(userNumOne, userNumTwo);
+        }
+    }
+}
