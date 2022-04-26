b3c7e7692d8c702368eaecdaea058db9a852bdd5
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-26 07:55:17 -0600

    Step 263 Try/Catch

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a number: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dividing the two...");
+            int quotient = numberOne / numberTwo;
+            Console.WriteLine($"{numberOne} divided by {numberTwo} is {quotient}.");
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message + "\nPlease enter only a number.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message + "\nEnter a number to divide by other than zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ Step263/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Populated list of numbers.
+            List<int> numbers = new List<int>() { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+            Console.WriteLine("Hello. Please enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // 'For' loop that'll divide our list items by the user input.
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int quotient = numbers[i] / userNumber;
+                Console.WriteLine($"{numbers[i]} divided by {userNumber} is {quotient}.");
+            }
+
+        // Catching user input of a string.
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message + "\nText input is invalid. Please enter a number.");
+        }
+
+        // Catching user input of a zero.
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message + "\nEnter a number other than zero to divide by.");
+        }
+
+        // Catching all other exceptions.
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+
+    }
+}
