7abe7e73769a8055663204adda0f3b26a6abfcd0
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-27 10:49:20 -0600

    Step 281, C# course.

---
+++ Step281/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Declaring and initializing our variables.
+            int firstNumber = 500;
+            int secondNumber = 100;
+
+            // Instantiating an object 'operate' of class 'Math'.
+            Math operate = new Math();
+
+            // Calling upon 'Add' method within class 'Math' with object 'operate' while passing in our variables.
+            operate.Add(firstNumber, secondNumber);
+            Console.ReadLine();
+        }
+    }
+}
