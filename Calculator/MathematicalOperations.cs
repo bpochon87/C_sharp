dd1d8d59778b3c34ec2086a7b45781ab43a9b67c
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-27 10:07:41 -0600

    Step 280 C# course.

--- Calculator/MathematicalOperations.cs
+++ Calculator/MathematicalOperations.cs
@@ -8,25 +8,10 @@ namespace Calculator
 {
     public class MathematicalOperations
     {
-        // Addition method.
-        public int Add(int userNumber)
+        // Addition method with required first parameter and optional second parameter.
+        public int Add(int firstNumber, int secondNumber = 0)
         {
-            int sum = userNumber + 10;
-            return sum;
-        }
-
-        // Double (decimal) addition method.
-        public int Add(double userDecimal)
-        {
-            double sum = userDecimal + 624.3;
-            return (int)sum;
-        }
-
-        // Multiplication method.
-        public int Add(string userString)
-        {
-            int userInteger = Convert.ToInt32(userString);
-            int sum = userInteger + 23850;
+            int sum = 500 + firstNumber + secondNumber;
             return sum;
         }
     }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,15 +12,11 @@ namespace Calculator
         {
             // Get user input as integer.
             Console.WriteLine("Please enter a number: ");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-            // Get user input as float.
-            Console.WriteLine("Now enter a decimal: ");
-            double userDecimal = Convert.ToDouble(Console.ReadLine());
-
-            // Get user input as string.
-            Console.WriteLine("...and finally another number: ");
-            string userString = Console.ReadLine();
+            // Get optional second user input.
+            Console.WriteLine("Please enter another number or enter zero if no whole number is desired: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
 
             // Instantiating new object of 'MathetmaticalOperations' class.
             // 'calculate' is an instance of the class MathematicalOperations.
@@ -28,9 +24,7 @@ namespace Calculator
 
             // We are able to pass in user input to each method within the 'calculate' instance because we're
             // now able to access all of the attributes of class MathetmaticalOperations.
-            Console.WriteLine($"{userNumber} added to ten is {calculate.Add(userNumber)}.");
-            Console.WriteLine($"Your number as a decimal added to 624.3 is " + calculate.Add(userDecimal) + ".");
-            Console.WriteLine($"{userString} added to 23850 is {calculate.Add(userString)}.");
+            Console.WriteLine($"{firstNumber} and {secondNumber} added to 500 is {calculate.Add(firstNumber, secondNumber)}.");
 
             Console.ReadLine();
         }
