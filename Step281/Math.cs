6293a6ddcd6da82fe662f7c34976e3dbb6eade5c
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-28 11:26:01 -0600

    Step 281, C# course refactor.

---
+++ Step281/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    public class Math
+    {
+        public void Add(int numberOne, int numberTwo)
+        {
+            int sum = numberOne + 500;
+            Console.WriteLine($"The second number is {numberTwo}.");
+        }
+    }
+}
---
+++ Step281/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating object of class 'Math'.
+            Math calculate = new Math();
+
+            // Passing two integers into the 'Add' method of 'Math' class.
+            calculate.Add(55, 100);
+
+            // Passing two integers via the named parameters of the 'Add' method in the 'Math' class.
+            calculate.Add(numberTwo: 500, numberOne: 500);
+
+            Console.ReadLine();
+        }
+    }
+}
