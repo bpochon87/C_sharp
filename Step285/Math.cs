6d8859051095a64ac683f30be33df0b63a01fcbe
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-04-29 10:13:48 -0600

    Step 285, C# course.

---
+++ Step285/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285
+{
+    public class Math
+    {
+        // This method accepts one argument and has one 'out' parameter.
+        public void Divide(double passedData1, out double quotient)
+        {
+            quotient = passedData1 / 2;
+        }
+
+        // Although this method isn't being used, we could ask for another input from the user and, through overloading,
+        // call this method instead of the above method.
+        public void Divide(double passedData1, double passedData2, out double quotient)
+        {
+            quotient = passedData1 / passedData2;
+        }
+    }
+    // This 'static class' is doing nothing but can act as a template for inherited classes.
+    static class Mathmematician
+    {
+        public static string FirstName;
+        public static string LastName;
+        public static string AreaExpertise;
+        public static string YearsExperience;
+    }
+}
---
+++ Step285/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math calculate = new Math();
+
+            // Get user input to pass into 'Math' class method 'Divide'.
+            Console.WriteLine("Please enter a number: ");
+            double passedData1 = Convert.ToDouble(Console.ReadLine());
+
+            // Calling on 'Math' class 'Divide' method while passing in one parameter.
+            // The 'out' parameter will allow a returned value from our 'Divide' method within 'Math' class.
+            calculate.Divide(passedData1, out double quotient);
+            Console.WriteLine($"{passedData1} divided by two is {quotient}.");
+            Console.ReadLine();
+
+        }
+    }
+}
