3a582782c5419f7bb3e04e164f65d4b5b9664078
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-05-02 18:32:00 -0600

    Step 313, C# course.

---
+++ Step313/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step313
+{
+    public class Employee
+    {
+        // Declaring class 'Employee' properties.
+        public string firstName;
+        public string lastName;
+        public int ID;
+
+        // Overloading the comparison operator. 
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.ID == employee2.ID)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+        // Because the == operator is used, the user-defined != comparison must also be defined.
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            bool status = false;
+            if (employee1.ID != employee2.ID)
+            {
+                status = true;
+            }
+            return status;
+        }
+    }
+}
---
+++ Step313/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing and instantiating objects of 'Employee' class.
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            // Assigning values to 'Employee' class properties.
+            employee1.firstName = "Sara";
+            employee2.firstName = "Brady";
+
+            employee1.lastName = "Pochon";
+            employee2.lastName = "Pochon";
+
+            employee1.ID = 1;
+            employee2.ID = 1;
+
+            // Because the bool comparison operator '==' and '!=' were user-defined within the 'Employee' class, we are able to
+            // compare two objects of the 'Employee' class on the 'ID' property.
+            if (employee1 == employee2)
+            {
+                Console.WriteLine($"The ID number {employee1.ID} is assigned to {employee1.firstName} and {employee2.firstName}.");
+            }
+            else
+            {
+                Console.WriteLine($"{employee1.firstName} and {employee2.firstName} have unique IDs.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
