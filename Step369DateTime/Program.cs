04c8a61fb9fd88a0761912aa2cce51b4ce89d80a
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-05-04 15:26:55 -0600

    Step 369, C# course: DateTime.

---
+++ Step369DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step369DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setting current time to variable.
+            var currentTime = DateTime.Now;
+
+            Console.WriteLine($"The current time is {currentTime}.\n");
+            Console.WriteLine("Now, please enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Assigning variable to 'userNumber' hours plus 'currentTime' hours.
+            DateTime futureTime = currentTime.AddHours(userNumber);
+            Console.WriteLine($"\nIn {userNumber} hours it will be {futureTime}.");
+            Console.ReadLine();
+        }
+    }
+}
