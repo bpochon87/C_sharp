67723ec7cf806fec2690fa6d0cfa2c88911afdfb
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-05-05 11:55:33 -0600

    Step 383, C# course: constructors.

---
+++ Step383Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383Constructor
+{
+    class Program
+    {
+        // Only C# built-in types (sans System.Object) may be declared const. User-defined (classes, strructs, arrays) cannot be const.
+        public const double Pi = 3.14159;
+
+        static void Main()
+        {            
+            var student = new Student(1, "Brady", "Pochon", 14);
+            Console.WriteLine($"Let's welcome {student.FirstName} {student.LastName} to grade {student.GradeLevel}!\n");
+            Console.WriteLine($"Does {student.FirstName} know about {Pi}? Let's hope so!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Step383Constructor/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383Constructor
+{
+    public class Student
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int GradeLevel;
+        
+        // Constructor chaining instead of overloading via ': this.(field)'.
+
+        public Student()
+        {
+        }
+
+        public Student(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Student(int id, string fName)
+            : this(id)
+        {
+            this.FirstName = fName;
+        }
+
+        public Student(int id, string fName, string lName)
+            : this(id, fName)
+        {
+            this.LastName = lName;
+        }
+
+        public Student(int id, string fName, string lName, int grade)
+            : this(id, fName, lName)
+        {
+            this.GradeLevel = grade;
+        }
+    }
+}
