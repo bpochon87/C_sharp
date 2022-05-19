5e7cfc621d062d32bc5c39ba981d5af517abf495
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-05-19 09:19:37 -0600

    No dB showing.

---
+++ FinalChallengeSubmission/FinalChallengeSubmission/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallengeSubmission
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentFullName { get; set; }
+        public int Age { get; set; }
+        public decimal GPA { get; set; }
+        public int GradeLevel { get; set; }
+    }
+
+    // Defining a derived context is necessary to represent a session with the dB.
+    // This allows us to query and save data.
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
